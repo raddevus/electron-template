5bac8e20b870ecabb3a9cb891cab6d13c817a8ef
raddevus <1radeutsch1@gmail.com>
2019-10-17 16:13:18 -0400

    Fixed package.json ref to electron-builder which needs to be in the devDependencies.  Added line to shell script so after the remote url is set the project will be pushed to master branch on new repo.

--- ChangeRemoteGitOrigin.sh
+++ ChangeRemoteGitOrigin.sh
@@ -1,1 +1,6 @@
+# $1 is your GitHub URL that will be used as the ORIGIN
 git remote set-url origin $1
+# now we push the code for the first time 
+# to your repo on the master branch
+git push -u origin master
+
