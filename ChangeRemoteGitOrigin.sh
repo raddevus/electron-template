f8726d655135bc66fc7c28871c5a56da98a4818d
raddevus <1radeutsch1@gmail.com>
2019-10-18 13:24:17 -0400

    Added line to remove README.md file from the repo since new project will need its own new README.md file.

--- ChangeRemoteGitOrigin.sh
+++ ChangeRemoteGitOrigin.sh
@@ -1,5 +1,7 @@
 # $1 is your GitHub URL that will be used as the ORIGIN
 git remote set-url origin $1
+# Delete the readme.md file since the new project will need a new one.
+rm README.md -f
 # now we push the code for the first time 
 # to your repo on the master branch
 git push -u origin master
